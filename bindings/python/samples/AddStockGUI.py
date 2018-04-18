c24a948498e9b954a677741366fd209ed7946fdc
SUPERharrison <hwelch1@my.apsu.edu>
2018-04-17 19:42:06 -0500

    now you can get text

--- bindings/python/samples/AddStockGUI.py
+++ bindings/python/samples/AddStockGUI.py
@@ -8,11 +8,13 @@ def getColor():
     color = askcolor()
     
 
-def addStock(stocks, symbol):
-    stocks.append({"symbol":symbolField.get(), "red":color[0][0], "green":color[0][1], "blue":color[0][2]})
-    f = open("stockConfig.py", "w+")
-    f.write("stocks = ")
-    f.write(str(stockConfig.stocks))
+def addStock(editText):
+    stock_text = editText.get()
+    print("stock_text = " + str(stock_text))
+    # stocks.append({"symbol":symbolField.get(), "red":color[0][0], "green":color[0][1], "blue":color[0][2]})
+    # f = open("stockConfig.py", "w+")
+    # f.write("stocks = ")
+    # f.write(str(stockConfig.stocks))
     
 
 stockList = stockConfig.stocks
@@ -21,5 +23,5 @@ Label(master, text="Stock Symbol").grid(row=0)
 symbolField = Entry(master)
 symbolField.grid(row=0,column=1)
 Button(text='Select Color', command=getColor).grid(row=1, column=1)
-Button(text='Add Stock', command=addStock(stockList, symbolField)).grid(row=2, column=1)
+Button(text='Add Stock', command=lambda: addStock(symbolField)).grid(row=2, column=1)
 mainloop()
